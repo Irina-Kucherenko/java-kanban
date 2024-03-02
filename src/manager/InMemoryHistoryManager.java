efaffc43a25fa859c77e9620c8579f3579fa30ba
Irina Kucherenko <raito-werner@yandex.ru>
2024-03-02 17:06:22 +0300

    Добавлены модификаторы доступа

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class InMemoryHistoryManager implements HistoryManager {
 
     private final HashMap<Integer, Node<Task>> recentHistory = new HashMap<>();
-    Node<Task> tail;
-    Node<Task> head;
+    private Node<Task> tail;
+    private Node<Task> head;
     @Override
     public List<Task> getHistory() {
         List<Task> taskList = new ArrayList<>();
@@ -28,11 +28,12 @@ public class InMemoryHistoryManager implements HistoryManager {
         linkLast(task.cloneObject()); /*Объясняю: метод cloneObject нужен для того, чтобы сохранилась в истории
         прошлая версия объекта. Допустим, ситуация: мы захотели положить в историю объект, положили; позже нам
         захотелось изменить название и описание объекта, вызвали get метод для определённого объекта. И всё у нас
-        сохранена первая версяя объекта и актуальная. Руководствовалась требованиями задания, не более*/
+        сохранена первая версия объекта и актуальная. Руководствовалась требованиями задания, не более*/
     }
 
     @Override
-    public void remove(int id) {
+    public void remove(int id) { /*непонятен Ваш комментарий, InMemoryHistoryManager это же
+    и есть реализация интерфейса*/
 
         if (recentHistory.containsKey(id)) {
             Node<Task> value = recentHistory.get(id);
